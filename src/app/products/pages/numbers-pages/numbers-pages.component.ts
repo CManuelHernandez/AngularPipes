0ae65bb7a18bf7f4d56dd6ffcbefcaf9004131dd
C.Manuel Hernández <cmanuelhm87@gmail.com>
2024-10-03 12:26:30 +0200

    Decimal, Currency y Percent Pipes

--- src/app/products/pages/numbers-pages/numbers-pages.component.ts
+++ src/app/products/pages/numbers-pages/numbers-pages.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-pages',
   templateUrl: './numbers-pages.component.html',
-  styleUrl: './numbers-pages.component.css'
+  styleUrl: './numbers-pages.component.css',
 })
 export class NumbersPagesComponent {
-
+  public totalSells: number = 2567789.5567;
+  public percent: number = 0.4856;
 }
